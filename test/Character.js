3364a7fb67ddf4f54194272ce38d279c1e6200a7
dedelhar <DEdelhart@svb.com>
2016-05-11 11:55:38 -0700

    generative deck functions

--- test/Character.js
+++ test/Character.js
@@ -3,35 +3,35 @@ import Character from '../lib/Character';
 const assert = chai.assert;
 
 describe('suits-rules', function () {
-    describe('Character', function () {
-        let char;
+  describe('Character', function () {
+    let char;
 
-        beforeEach(() => {
-            char = new Character({
-                name: 'foo',
-                body: 4,
-                mind: 5,
-                speed: 6,
-                spirit: 7
-            });
-        });
-
-        describe('attributes', () => {
-            it('should set body', () => assert.equal(char.body, 4, 'body is set'));
-            it('should set mind', () => assert.equal(char.mind, 5, 'mind is set'));
-            it('should set speed', () => assert.equal(char.speed, 6, 'speed is set'));
-            it('should set spirit', () => assert.equal(char.spirit, 7, 'spirit is set'));
-        });
-
-        describe('name', () => {
-            it('should set name', () => assert.equal(char.name, 'foo', 'name is set'));
-        });
-
-        describe('skills', () => {
-            beforeEach(() => char.addSkill('running', 'body', 2));
-
-            it('should have skill rank 6', () => assert.equal(char.skillRank('running'), 6, 'gets the right rank'));
-            it('should get rank 0 for non-set skill', () => assert.equal(char.skillRank('acting'), 0, 'unset skill is zero'));
-        });
+    beforeEach(() => {
+      char = new Character({
+        name: 'foo',
+        body: 4,
+        mind: 5,
+        reflexes: 6,
+        spirit: 7
+      });
     });
+
+    describe('attributes', () => {
+      it('should set body', () => assert.equal(char.body, 4, 'body is set'));
+      it('should set mind', () => assert.equal(char.mind, 5, 'mind is set'));
+      it('should set reflexes', () => assert.equal(char.reflexes, 6, 'reflexes is set'));
+      it('should set spirit', () => assert.equal(char.spirit, 7, 'spirit is set'));
+    });
+
+    describe('name', () => {
+      it('should set name', () => assert.equal(char.name, 'foo', 'name is set'));
+    });
+
+    describe('addSkill', () => {
+      beforeEach(() => char.addSkill('running', 'body', 2));
+
+      it('should have skill rank 6', () => assert.equal(char.skillRank('running'), 6, 'gets the right rank'));
+      it('should get rank 0 for non-set skill', () => assert.equal(char.skillRank('acting'), 0, 'unset skill is zero'));
+    });
+  });
 });
