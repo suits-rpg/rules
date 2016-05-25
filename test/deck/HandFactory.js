9589df1f6b3c8d63df6b114ed2d3208072e02579
Dave Edelhart <dave@wonderlandlabs.com>
2016-05-25 12:13:41 -0700

    continued validation of HandFactory

--- test/deck/HandFactory.js
+++ test/deck/HandFactory.js
@@ -6,8 +6,8 @@ import Card from '../../lib/deck/Card';
 
 const assert = chai.assert;
 
-describe ('HandFactory', () => {
-   let hand;
+describe('HandFactory', () => {
+    let hand;
 
     beforeEach(() => {
         hand = new HandFactory(index => {
@@ -17,10 +17,31 @@ describe ('HandFactory', () => {
         });
     });
 
-    it('should return the predicted first card', () => {
+    it('.card', () => {
         let card = hand.card();
 
         assert.equal(card.value, 'A');
         assert.equal(card.suit, SUITS[0]);
+    });
+
+    describe('.cards', () => {
+        let cards;
+        beforeEach(() => {
+             cards = hand.cards(3);
+        });
+
+it('should provide the requested count of cards', () => {
+    assert.equal(cards.length, 3);
+});
+
+        it('should provide the predicted cards', () => {
+            let card = cards[0];
+            assert.equal(card.value, 'A');
+            assert.equal(card.suit, SUITS[0]);
+
+             card = cards[2];
+            assert.equal(card.value, 5);
+            assert.equal(card.suit, SUITS[2]);
+        });
     });
 });
